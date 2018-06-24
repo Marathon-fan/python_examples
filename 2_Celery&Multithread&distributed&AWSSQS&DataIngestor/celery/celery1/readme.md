e6d0a5d01eaa842e5f4771efb86a46632095b0f6
John Zhou <johnprogrammer2005@gmail.com>
2018-06-25 08:24:13 +1000

    add celery and multithread example

---
+++ 2_Celery&Multithread&distributed&AWSSQS&DataIngestor/celery/celery1/readme.md
@@ -0,0 +1,14 @@
+
+
+how to use it
+```
+in window1
+
+celery -A tasks worker --loglevel=info
+
+in window2
+
+python3 invoker.py
+
+
+```
---
+++ 2_Celery&Multithread&distributed&AWSSQS&DataIngestor/celery/celery1/tasks.py
@@ -0,0 +1,8 @@
+from celery import Celery
+
+app = Celery('tasks', backend=None, broker='redis://localhost:6379/0')
+
+@app.task(name='tasks.add')
+def add(x, y):
+    print('{} + {} = {}'.format(x, y, x + y))
+
